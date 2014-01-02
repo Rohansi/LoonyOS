60734bd19ed06b0e87f1ef9748598b8cd00ec8f8
Rohan Singh <rohan-singh@hotmail.com>
2014-01-02 16:34:08 -0500

    Update dskimg

--- install.lua
+++ install.lua
@@ -1,29 +1,26 @@
 
--- Copy bootloader
 LoadDriver("RAW.dll");
-if Mount("bin/disk.img") then
-    -- copy boot sector
-    RawWrite("bin/boot.bin", 0, 0, 6);
-    RawWrite("bin/boot.bin", 90, 90, 422);
+Mount("bin/disk.img")
 
-    -- copy bootloader
-    RawWrite("bin/boot.bin", 512, 16 * 512, 4 * 512);
-else
-    return
-end
+-- copy boot sector
+RawWrite("bin/boot.bin", 0, 0, 6);
+RawWrite("bin/boot.bin", 90, 90, 422);
+
+-- copy bootloader
+RawWrite("bin/boot.bin", 512, 16 * 512, 4 * 512);
+
 Dismount();
 
 
--- Copy files
 LoadDriver("FAT32.dll");
-if Mount("bin/disk.img") then
+Mount("bin/disk.img")
 
-    DirCreate("loony")
-    ChDir("/loony/")
-    
-    CopyIn("bin/kernel.bin", "kernel.bin")
+CopyOut("test", "out.bin")
 
-else
-    return
-end
+DirCreate("loony")
+ChDir("/loony/")
+
+CopyIn("bin/kernel.bin", "kernel.bin")
+
 Dismount();
+
